1089a1e426b23b1959cc1ba65d34fc420ac09033
Castelian <leo@sqrdao.com>
2025-04-29 16:11:15 +0700

    add Contract address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const ARISE_CONTRACT_ADDRESS = '0x1234567890123456789012345678901234567890'; // Replace with actual contract address
+export const ARISE_CONTRACT_ADDRESS = '0x696c2Da4D0D8E1C8e1c8EaC2d068238dc99821Cf';
 
 export const ARISE_CONTRACT_ABI = [
   {
